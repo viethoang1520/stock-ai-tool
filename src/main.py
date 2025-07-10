b75e8d92e670289fb30eb01c608491b8a93b9744
viethoang1520 <viethoang1520.dev@gmail.com>
2025-07-10 22:24:21 +0700

    fix analysis path reference

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI
 from pydantic import BaseModel
 import asyncpg
